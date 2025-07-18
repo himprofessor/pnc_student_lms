52f98b5394b37d3da4793adc9bbee16fb5ae1f85
Sreyvang <sreyvang.phon@student.passerellesnumeriques.org>
2025-07-18 15:22:57 +0700

    congig tailwind

--- web_frontend/src/App.vue
+++ web_frontend/src/App.vue
@@ -1,11 +1,9 @@
 <template>
-  <div class="min-h-screen bg-gray-100">
-    <Navbar />
-  </div>
+  
 </template>
 
 <script setup>
-import Navbar from './components/layouts/Navbar.vue';
+
 
 </script>
 
