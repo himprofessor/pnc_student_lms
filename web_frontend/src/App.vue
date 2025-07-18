5c74b75f2f33ce98321fc3fdd88ac42af77bcfdd
selatorm <sela.torm@student.passerellesnumeriques.org>
2025-07-18 15:48:37 +0700

    CPSL-19 | frontend: Save work before merging

--- web_frontend/src/App.vue
+++ web_frontend/src/App.vue
@@ -1,16 +1,9 @@
 <template>
-  <div class="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-    <h1 class="text-4xl font-bold text-gray-800 mb-4">Welcome to PNC Student LMS</h1>
-    <p class="text-lg text-gray-600 mb-8">Your learning management system powered by Vue & Tailwind CSS.</p>
-    <button class="px-6 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition">Get Started</button>
-  </div>
+  <ProfileSettings />
 </template>
 
 <script setup>
-
+import ProfileSettings from "./views/ProfileSettings.vue";
+// import ProfileSettings from "../views/ProfileSettings.vue";
 
 </script>
-
-<style scoped>
-
-</style>
