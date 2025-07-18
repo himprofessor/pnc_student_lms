66a58bf115893ba4f744c5303573a34b4c0119a3
selatorm <sela.torm@student.passerellesnumeriques.org>
2025-07-18 16:29:56 +0700

    CPSL-19 | frontend: Resolve merge conflict in App.vue

--- web_frontend/src/App.vue
+++ web_frontend/src/App.vue
@@ -1,12 +1,11 @@
 <template>
   <div class="min-h-screen bg-gray-100">
     <Navbar />
+    <ProfileSettings />
   </div>
 </template>
 
 <script setup>
 import ProfileSettings from "./views/ProfileSettings.vue";
 import Navbar from "./components/layouts/Navbar.vue";
-
-
 </script>
