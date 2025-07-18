5df8ec92186a93e7a087eedbc62170395c2301a1
Sreyvang <sreyvang.phon@student.passerellesnumeriques.org>
2025-07-18 15:33:19 +0700

    develop

--- web_frontend/src/App.vue
+++ web_frontend/src/App.vue
@@ -1,8 +1,11 @@
 <template>
-  
+  <div class="min-h-screen bg-gray-100">
+    <Navbar />
+  </div>
 </template>
 
 <script setup>
+import Navbar from './components/layouts/Navbar.vue';
 
 
 </script>
