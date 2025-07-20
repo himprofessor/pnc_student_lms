4c7df4aec0fdac2788799808f3bd67f4da808222
mesa <natmesa12a@gmail.com>
2025-07-20 12:31:31 +0700

    CPSL-3 | frontend: delete navbar on athu page

--- web_frontend/src/router/index.js
+++ web_frontend/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router' 
 import Home from '../views/HomeView.vue'
 import Login from '../views/Auth/Login.vue'
 import Register from '../views/Auth/Register.vue'
@@ -12,8 +12,8 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { path: '/', component: Home },
-    { path: '/login', component: Login },
-    { path: '/register', component: Register },
+    { path: '/login', component: Login, meta: { hideNavbar: true } },
+    { path: '/register', component: Register, meta: { hideNavbar: true } },
     { path: '/dashboard', component: DashboardPage },
     { path: '/request-leave', component: RequestLeavePage },
     { path: '/history', component: HistoryPage },
