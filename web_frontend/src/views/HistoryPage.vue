9173556d302518d03c04b2621689f80656ecfdb0
neaksina <sina.neak@student.passerellesnumeriques.org>
2025-07-22 14:00:09 +0700

    CPSL-9 | change color button on the detials

--- web_frontend/src/views/HistoryPage.vue
+++ web_frontend/src/views/HistoryPage.vue
@@ -163,7 +163,7 @@
       <!-- Footer -->
       <div class="mt-6 text-center">
         <button @click="showModal = false"
-          class="bg-gradient-to-r from-red-500 to-indigo-500 text-white px-6 py-2 rounded-xl font-semibold hover:scale-105 transition">
+          class="bg-gradient-to-r from-red-700 to-indigo-500 text-white px-6 py-2 rounded-xl font-semibold hover:scale-105 transition">
           Close
         </button>
       </div>
