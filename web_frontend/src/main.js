a816a0a891b0ee43a42a9b2cedd035c85f2a2997
selatorm <sela.torm@student.passerellesnumeriques.org>
2025-07-24 08:39:45 +0700

    Save changes before switching branch

--- web_frontend/src/main.js
+++ web_frontend/src/main.js
@@ -3,7 +3,7 @@ import './assets/main.css';
 
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
-// import axios from 'axios';
+import axios from 'axios';
 
 import App from './App.vue';
 import router from './router';
