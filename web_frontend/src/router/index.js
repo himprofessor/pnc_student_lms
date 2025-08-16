ba0829b549b1d74c5fbe6ae7edaba12174f6876c
Darin_HOY <darin.hoy@student.passerellesnumeriques.org>
2025-08-16 12:11:33 +0700

    CPSL-27 | frontend: Implement calendar for the student

--- web_frontend/src/router/index.js
+++ web_frontend/src/router/index.js
@@ -8,6 +8,7 @@ import DashboardPage from '@/views/student/DashboardPage.vue'
 import RequestLeavePage from '@/views/student/RequestLeavePage.vue'
 import HistoryPage from '@/views/student/HistoryPage.vue'
 import ProfilePage from '@/views/student/ProfilePage.vue'
+import CalendarPage from '@/views/student/CalendarPage.vue'
 
 import EducatorDashboard from '@/views/educator/EducatorDashboard.vue'
 import EducatorHistory from '@/views/educator/EducatorHistory.vue'
@@ -23,6 +24,7 @@ const routes = [
   { path: '/request-leave', component: RequestLeavePage, meta: { requiresAuth: true, role: 'student' } },
   { path: '/history', component: HistoryPage, meta: { requiresAuth: true, role: 'student' } },
   { path: '/profile', component: ProfilePage, meta: { requiresAuth: true, role: 'student' } },
+  { path: '/calendar', component: CalendarPage, meta: { requiresAuth: true, role: 'student' } },
 
   // Teacher routes
   { path: '/educator-dashboard', component: EducatorDashboard, meta: { requiresAuth: true, role: 'teacher' } },
--- web_frontend/src/views/student/DashboardPage.vue
+++ web_frontend/src/views/student/DashboardPage.vue
@@ -31,6 +31,14 @@
           </p>
         </div>
         <div class="flex items-center space-x-4">
+           <!-- 📅 Calendar Button -->
+          <router-link to="/calendar"
+            class="relative p-2 text-gray-600 hover:text-gray-800 hover:bg-gray-100 rounded-full transition-colors">
+            <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
+            </svg>
+          </router-link>
           <!-- Notification Icon -->
           <div class="relative">
             <button
